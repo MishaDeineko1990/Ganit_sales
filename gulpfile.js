0ec3335c59bf0792d15782b35fd930743ce62f89
deinekomisha1990 <deinekomisha1990@gmail.com>
2019-07-04 23:47:03 +0300

    23:45 work from gulp will continue

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+let cleanCSS = require('gulp-clean-css')
+let uglify =require('gulp-uglify')
 
 const css_files = [
 	'./src/tamp_styles/1_head.css',
@@ -14,6 +16,16 @@ const css_files = [
 	'./src/tamp_styles/999_adaptation.css'	
 ]
 
+const jsFiles = [
+	'./src/tamp_script/1_start.js',
+	'./src/tamp_script/2_work_from_languige.js',
+	'./src/tamp_script/3_use_google_table.js',
+	'./src/tamp_script/4_menu_navigation.js',
+	'./src/tamp_script/5_overlay.js',
+	'./src/tamp_script/999_finish.js'	
+]
+
+
 //task for styles css 
  function styles() {
  	return gulp.src(css_files)
@@ -24,6 +36,10 @@ const css_files = [
 		browsers: ['last 2 versions'],
 		cascade: false
 	}))
+
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+
+//вигрузка файлу
 	.pipe(gulp.dest('./src/styles')) 
 
  }
@@ -34,7 +50,16 @@ const css_files = [
 //task for styles scripts 
 function scripts(){
 
+	return gulp.src(jsFiles)
 
+ 	.pipe(concat('js.js'))
+
+ 	.pipe(uglify({
+ 		toplevel: true
+ 	}))
+
+//вигрузка файлу
+	.pipe(gulp.dest('./src/script')) 
 }
 
 //task for get function styles
